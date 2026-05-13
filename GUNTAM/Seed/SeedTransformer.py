bca989c5a003e63ed18769757c5b08972644e26f
Corentin Allaire <corentin.allaire@cern.ch>
2026-05-13 20:47:45 +0200

    masking the extra eta

--- GUNTAM/Seed/SeedTransformer.py
+++ GUNTAM/Seed/SeedTransformer.py
@@ -145,7 +145,7 @@ class SeedTransformer(nn.Module):
         z0_vals = torch.tensor([-200, -150.0, -100.0, -50.0, 50.0, 100.0, 150.0, 200], dtype=hits.dtype, device=hits.device)
         r = hits[..., 3].unsqueeze(-1)  # [..., n_hits, 1]
         dz = hits[..., 2].unsqueeze(-1) - z0_vals  # [..., n_hits, 8]
-        eta_z0 = torch.arctanh(dz / torch.sqrt(r ** 2 + dz ** 2 + 1e-8))  # [..., n_hits, 8]
+        eta_z0 = torch.arctanh(dz / torch.sqrt(r ** 2 + dz ** 2))  # [..., n_hits, 8]
         if mask is not None and mask.dim() == 2:
             eta_z0 = eta_z0.masked_fill(mask.unsqueeze(-1), 0.0)
         high_level = torch.cat([high_level, eta_z0], dim=-1) if high_level is not None else eta_z0
